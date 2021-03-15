44e4716686ff3cdbe7f9fc5f5cbd0f4f2dbce159
Amzinai Alkanas <amzinai.alkanas@gmail.com>
2021-03-15 21:30:47 +0200

    Adding contacts section

--- src/components/aboutSection/aboutSection.component.jsx
+++ src/components/aboutSection/aboutSection.component.jsx
@@ -1,9 +1,62 @@
 import React from "react";
 
-import { AboutContainer } from "./aboutSection.styles";
+import {
+  AboutContainer,
+  AboutWrapper,
+  AuthorContainer,
+  AuthorPortrait,
+  SocialsContainer,
+  SocialIcon,
+  ContactContainer,
+  ContactFormWrapper,
+  ContactInputText,
+  ContactInputTextarea,
+  ContactInputSubmit,
+  AboutTitle,
+} from "./aboutSection.styles";
 
 const AboutSection = () => (
-  <AboutContainer>ABOUT SECTION (under construction...)</AboutContainer>
+  <AboutContainer>
+    <AboutWrapper>
+      <AuthorContainer>
+        <AboutTitle>APIE MANE</AboutTitle>
+        <AuthorPortrait />
+        <p>
+          Jeigu mes dar nesusipažinome - LABAS! Aš esu Robertas. Maistas man yra
+          aistra - mėgstu skaniai ir kokybiškai pasigaminti maistą, tačiau taip
+          pat mėgstu aplankyti įvairius restoranus, paragauti neragautų skonių,
+          atrasti naujų patiekalų bei kokybiškai pavalgyti. Jeigu maisto
+          ragavimas ar gaminimas yra ir TAVO aistra - tuomet Sveikas, naujasis
+          bičiuli!
+        </p>
+      </AuthorContainer>
+      <SocialsContainer>
+        <AboutTitle>SUSISIEKIME</AboutTitle>
+        <SocialIcon />
+        <SocialIcon />
+        <SocialIcon />
+        <SocialIcon />
+      </SocialsContainer>
+      <ContactContainer>
+        <AboutTitle>PARAŠYK MAN:</AboutTitle>
+        <ContactFormWrapper>
+          <ContactInputText
+            inputType="title"
+            type="text"
+            placeholder="Tema..."
+            name="email"
+          />
+          <ContactInputTextarea
+            inputType="message"
+            type="text"
+            placeholder="Žinutė..."
+            name="message"
+          />
+          <ContactInputSubmit type="submit" value="Siųsti" />
+        </ContactFormWrapper>
+      </ContactContainer>
+    </AboutWrapper>
+  </AboutContainer>
 );
 
 export default AboutSection;
--- src/components/aboutSection/aboutSection.styles.jsx
+++ src/components/aboutSection/aboutSection.styles.jsx
@@ -1,12 +1,139 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
-  display: flex;
-  width: 100%;
   height: 800px;
-  justify-content: center;
-  align-items: center;
   color: white;
   background: black;
-  font-size: 40px;
+`;
+
+export const AboutWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 125px 340px;
+`;
+
+export const AuthorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+
+  p {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
+export const AuthorPortrait = styled.img`
+  width: 175px;
+  height: 175px;
+  border-radius: 50%;
+  background: red;
+  margin-bottom: 20px;
+`;
+
+export const SocialsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px;
+  height: 100%;
+`;
+
+export const SocialIcon = styled.div`
+  width: 67px;
+  height: 67px;
+  background-image: url("../../assets/facebook.png"); // TODO: work on adding pictures
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-color: white;
+  border: 3px solid red;
+  border-radius: 50%;
+  margin-bottom: 40px;
+`;
+
+export const ContactContainer = styled.div`
+  width: 400px;
+  height: 100%;
+`;
+
+export const ContactFormWrapper = styled.form``;
+
+export const ContactInputText = styled.input`
+  width: 100%;
+  height: ${(props) => (props.inputType === "title" ? "60px" : "230px")};
+  border: 3px solid white;
+  border-radius: 15px;
+  box-sizing: border-box;
+  background: none;
+  margin-bottom: 20px;
+  padding-left: 20px;
+  color: white;
+  font-size: 18px;
+
+  ::placeholder {
+    font-size: 18px;
+    text-align: start;
+    color: #7a7a7a;
+    opacity: 1;
+  }
+
+  :focus {
+    outline: none;
+  }
+`;
+
+export const ContactInputTextarea = styled.textarea`
+  width: 100%;
+  height: 230px;
+  border: 3px solid white;
+  border-radius: 15px;
+  box-sizing: border-box;
+  background: none;
+  margin-bottom: 20px;
+  padding-left: 20px;
+  padding-top: 15px;
+  color: white;
+  font-family: inherit;
+  font-size: 18px;
+
+  ::placeholder {
+    font-size: 18px;
+    text-align: start;
+    color: #7a7a7a;
+    opacity: 1;
+  }
+
+  :focus {
+    outline: none;
+  }
+`;
+
+// TODO: turn to button
+export const ContactInputSubmit = styled.input`
+  display: flex;
+  width: 132px;
+  height: 46px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 23px;
+  background: red;
+  font-size: 18px;
+  color: white;
+  border: 0;
+
+  :focus {
+    outline: none;
+  }
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const AboutTitle = styled.div`
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 70px;
 `;
